b6896c01f351a3b388383277d4c17be068db0150
Shreyas Lakshman Swamy <shreyasl2515@gmail.com>
2023-05-23 16:19:52 +0530

    Second commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDH_ZkdJfTIIzo-1Bnj5Brth1K2DbPWcx4',
+  authDomain: 'movie-search-9db88.firebaseapp.com',
+  projectId: 'movie-search-9db88',
+  storageBucket: 'movie-search-9db88.appspot.com',
+  messagingSenderId: '478939373499',
+  appId: '1:478939373499:web:312469126c50de1f17ee9e',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
